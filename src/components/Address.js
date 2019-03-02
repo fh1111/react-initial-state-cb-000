d25b6aab6010a337aebce70b4edc00bf371dbf90
Fer Hernandez <fh.1111@gmail.com>
2019-03-02 17:27:09 +0000

    Automatically backed up by Learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,16 +1,16 @@
 // Component Code Goes Here
 
 import React from 'react';
- 
+
 class Address extends React.Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
       fullAddress: `${props.street}, ${props.city}`
     }
-  } 
- 
+  }
+
   render() {
     return (
       <div className="address">
@@ -19,5 +19,5 @@ class Address extends React.Component {
     );
   }
 }
- 
+
 export default Address;
